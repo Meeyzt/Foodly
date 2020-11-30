5e18f740daf05ac74eb5970776f328a337c04f83
Meeyzt <mehmetbs@hotmail.com.tr>
2020-11-30 12:34:38 +0300

    init project

---
+++ Foodly/Models/DomainModels/Review.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Foodly.Models
+{
+    public class Review
+    {
+        [Key,Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReviewID { get; set; }
+        [Required]
+        public string Header { get; set; }
+        [Required]
+        public string Adress { get; set; }
+        [Required]
+        public string Blog { get; set; }
+        [Required]
+        public string PictureURL { get; set; }
+        [Required]
+        public string RestaurantName { get; set; }
+        [Required]
+        public double Star { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public DateTime PublishDate { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public bool Publish { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Foodly/Models/DomainModels/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Foodly.Models
+{
+    public class User
+    {
+        [Key,Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required,DataType(DataType.Password)]
+        public int Password { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        public string DisplayName { get; set; }
+        [Required]
+        public DateTime RegisterDate { get; set; }
+        [Required]
+        public string SecretKey { get; set; }
+        [Required]
+        public string Auth { get; set; }
+    }
+}
