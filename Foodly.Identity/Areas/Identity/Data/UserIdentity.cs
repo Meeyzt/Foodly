14ae7f4b26d689974376b5ef92f3d15f93dde70a
Meeyzt <mehmetbs@hotmail.com.tr>
2020-12-13 18:55:20 +0300

    İdentity Configuration

---
+++ Foodly.Identity/Areas/Identity/Data/UserIdentity.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Foodly.Identity.Areas.Identity.Data
+{
+    public class UserIdentity:IdentityUser
+    {
+    }
+}
--- Foodly.Identity/Areas/Identity/IdentityHostingStartup.cs
+++ Foodly.Identity/Areas/Identity/IdentityHostingStartup.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using Foodly.Identity.Data;
+﻿using Foodly.Identity.Areas.Identity.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +13,20 @@ namespace Foodly.Identity.Areas.Identity
         public void Configure(IWebHostBuilder builder)
         {
             builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserIdentityContextConnection")));
+
+                services.AddDefaultIdentity<UserIdentity>(options => {
+                    options.SignIn.RequireConfirmedAccount = true;
+                    options.Password.RequiredLength = 8;
+                    options.User.RequireUniqueEmail = true;
+                    options.User.AllowedUserNameCharacters = "abcçdefghiıjklmnoöpqrsştuüvwxyzABCÇDEFGHIİJKLMNOÖPQRSŞTUÜVWXYZ0123456789";
+                })
+                        .AddRoles<IdentityRole>()
+                        .AddEntityFrameworkStores<UserIdentityContext>();
+
+                services.AddDistributedMemoryCache();
             });
         }
     }
