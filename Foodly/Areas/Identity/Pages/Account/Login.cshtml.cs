5cdecdfbe8a4092978d28df47b5d9c01bce04178
Meeyzt <mehmetbs@hotmail.com.tr>
2020-12-13 17:38:37 +0300

    Revert "Views Update"
    This reverts commit 6ee834db80d9653011cac2b7d04a9b0adf4eb1a1.

--- Foodly/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Foodly/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -41,7 +41,8 @@ namespace Foodly.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            public string UserName { get; set; }
+            [EmailAddress]
+            public string Email { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -76,7 +77,7 @@ namespace Foodly.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("Giriş Yapıldı.");
